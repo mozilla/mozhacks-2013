9933a3c4bf490a7a18ec8f9eab8aab8781f0e5b9
Trisha Salas <trisha@trishasalas.com>
2013-09-21 19:46:05 -0500

    Initial Commit for MDN Proto Theme

--- themes/Hacks2013/footer.php
+++ themes/Hacks2013/footer.php
@@ -1,35 +1,35 @@
 </div><!-- /#content -->
-
-<footer id="site-info">
-  <nav id="nav-legal">
-    <ul>
-      <li><a href="http://www.mozilla.com/privacy-policy.html" rel="external">Privacy Policy</a></li>
-      <li><a href="http://www.mozilla.com/about/legal.html" rel="external">Legal Notices</a></li>
-      <li><a href="http://www.mozilla.com/legal/fraud-report/" rel="external">Report Trademark Abuse</a></li>
-    </ul>
-  </nav>
-
-  <p id="copyright">Except where otherwise noted, content on this site is licensed under the <br /><a href="http://creativecommons.org/licenses/by-sa/3.0/" rel="license external">Creative Commons Attribution Share-Alike License v3.0</a> or any later version.</p>
-
-  <nav id="nav-footer">
-    <h5>hacks.mozilla.org:</h5>
-    <ul role="navigation">
-      <li><a href="<?php bloginfo('url'); ?>">Home</a></li>
-      <li><a href="<?php echo get_permalink(get_page_by_path('articles')->ID); ?>">Articles</a></li>
-      <li><a href="<?php echo get_permalink(get_page_by_path('demos')->ID); ?>">Demos</a></li>
-      <li><a href="<?php echo get_permalink(get_page_by_path('about')->ID); ?>">About</a></li>
-    </ul>
-  </nav>
+<section id="footbar">
+<div class="wrap">
+  <p>
+  Is MDN helpful to you? Please <a href="https://developer.mozilla.org/en-US/docs/Project:Feedback">share your feedback</a> with us. Or join our <a href="//lists.mozilla.org/listinfo/dev-mdc" id="dev-mdc-link">mailing list about improving MDN content</a>.  </p>
+</div>
+</section>
+<footer class="footer" id="site-info" role="contentinfo">
+<div class="wrap">
+    <div id="legal">
+      <img alt="" height="48" src="https://developer.mozilla.org/media/img/mdn-logo-tiny.png" width="42"></img>
+      <p id="copyright">© 2005 - 2013 Mozilla Developer Network and individual contributors</p>
+      <p>
+      Content is available under <a href="https://developer.mozilla.org/en-US/docs/Project:Copyrights">these licenses</a>      • <a href="https://developer.mozilla.org/en-US/docs/Project:About">About MDN</a>
+      • <a href="//github.com/mozilla/kuma">Contribute to the code</a>
+      • <a href="//www.mozilla.org/en-US/privacy">Privacy Policy</a>
+    </p></div>
+    </div>
 </footer>
 
-<script src="https://www.mozilla.org/tabzilla/media/js/tabzilla.js"></script>
-<script>
-// <![CDATA[
-  jQuery(document).ready(function(){
-    jQuery(document.body).addClass("js");
-  });
-// ]]>
-</script>
+<script src="https://developer.mozilla.org/jsi18n/build:e8ce54a"></script>
+
+        <script async src="//mozorg.cdn.mozilla.net/en-US/tabzilla/tabzilla.js"></script>
+      <script src="//www.google.com/jsapi" type="text/javascript"></script>
+      <script src="https://login.persona.org/include.js" type="text/javascript"></script>
+
+      
+      <script src="https://developer.mozilla.org/media/js/libs/jquery-1.9.1.js"></script>
+<script src="https://developer.mozilla.org/media/js/jquery-upgrade-compat.js"></script>
+<script src="https://developer.mozilla.org/media/js/init.js"></script>
+<script src="https://developer.mozilla.org/media/js/gsearch.js"></script>
+<script src="https://developer.mozilla.org/media/js/libs/jquery.cycle.js"></script>
 
 <?php wp_footer(); ?>
 </div>
--- themes/Hacks2013/header.php
+++ themes/Hacks2013/header.php
@@ -1,37 +1,74 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> id="hacks-mozilla-org">
+<html <?php language_attributes(); ?>>
 <head>
-  <meta name="viewport" content="width=1024">
   <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <!--[if IE]>
+  <meta http-equiv="imagetoolbar" content="no">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <meta name="Rating" content="General">
+  <meta name="MSSmartTagsPreventParsing" content="true">
 
-  <!-- OpenGraph metadata -->
+  <meta name="viewport" content="width=device-width">
+
+ <!-- For Facebook -->
   <meta property="og:site_name" content="<?php bloginfo('name'); ?>">
   <meta property="og:title" content="<?php if (is_singular()) : single_post_title(); else : bloginfo('name'); endif; ?>">
   <meta property="og:url" content="<?php if (is_singular()) : the_permalink(); else : bloginfo('url'); endif; ?>">
   <meta property="og:description" content="<?php fc_meta_desc(); ?>">
+  <meta property="og:image" content="<?php bloginfo('stylesheet_directory'); ?>/img/Q.png">
 
-  <!--[if IE]>
-  <meta name="MSSmartTagsPreventParsing" content="true">
+  <meta name="title" content="<?php if (is_singular()) : single_post_title(); echo ' | '; endif; bloginfo('name'); ?>">
+  <meta name="description" content="<?php fc_meta_desc(); ?>">
+
+   <!--[if !IE 6]><!-->
+      <link href="https://developer.mozilla.org/media/css/fonts.css" media="screen,projection,tv" rel="stylesheet"></link>
+<link href="https://developer.mozilla.org/media/css/mdn-screen.css" media="screen,projection,tv" rel="stylesheet"></link>
+<link href="https://developer.mozilla.org/media/css/video-player.css" media="screen,projection,tv" rel="stylesheet"></link>
+<link href="https://developer.mozilla.org/media/css/mdn-calendar.css" media="screen,projection,tv" rel="stylesheet"></link>
+<link href="https://developer.mozilla.org/media/css/redesign-transition.css" media="screen,projection,tv" rel="stylesheet"></link>
+            <!--<![endif]-->
+      <!--[if IE]><link rel="stylesheet" type="text/css" media="all" href="/media/css/ie.css" /><![endif]-->
+      <!--[if lte IE 7]><link rel="stylesheet" type="text/css" media="all" href="/media/css/ie7.css" /><![endif]-->
+      <!--[if lte IE 6]><link rel="stylesheet" type="text/css" media="all" href="/media/css/ie6.css" /><![endif]-->
+      <link href="https://developer.mozilla.org/media/css/libs/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"></link>
+      <link href="https://developer.mozilla.org/media/css/mdn-print.css" media="print" rel="stylesheet" type="text/css"></link>
+      <link href="//mozorg.cdn.mozilla.net/media/css/tabzilla-min.css" rel="stylesheet"></link>
+
+      <link href="https://developer-local.allizom.org/en-US/search/xml" rel="search" title="Mozilla Developer Network" type="application/opensearchdescription+xml"></link>
+<!--[if IE]>
   <meta http-equiv="imagetoolbar" content="no">
   <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <script src="https://developer-local.allizom.org//media/js/libs/html5.js"></script>
   <![endif]-->
 
-  <link rel="shortcut icon" type="image/ico" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico">
-  <link rel="home" href="/">
-  <link rel="copyright" href="#copyright">
-  <link rel="stylesheet" type="text/css" media="screen,projection" href="<?php bloginfo('stylesheet_url'); ?>">
-  <link rel="stylesheet" type="text/css" media="print,handheld" href="<?php echo get_template_directory_uri(); ?>/css/print.css">
-  <link rel="stylesheet" type="text/css" media="all" href="https://www.mozilla.org/tabzilla/media/css/tabzilla.css">
-  <?php if ( get_option('mozhacks_share_posts') ) : ?>
-  <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_template_directory_uri(); ?>/css/socialshare.css">
-  <?php endif; ?>
-  <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>">
-  <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-  <?php if (is_singular()) : ?><link rel="canonical" href="<?php echo the_permalink(); ?>"><?php endif; ?>
+  <script type="text/javascript">
+  // This represents the site configuration
+  window.mdn = {
+    build: 'e8ce54a',
+    ckeditor: {},
+    mediaPath: '/media/',
+    wiki: {
+      autosuggestTitleUrl: '/docs/get-documents'
+    }
+  };
+  // Ensures gettext always returns something, is always set
+  window.gettext = function(x) { return x; }
+</script>
+      <link href="https://developer.mozilla.org/media/css/wiki.css" media="screen,projection,tv" rel="stylesheet"></link>
+<link href="https://developer.mozilla.org/media/css/wiki-screen.css" media="screen,projection,tv" rel="stylesheet"></link>
+<link href="https://developer.mozilla.org/media/js/libs/jquery-ui-1.10.3.custom/css/ui-lightness/jquery-ui-1.10.3.custom.min.css" media="screen,projection,tv" rel="stylesheet"></link>
+<link href="https://developer.mozilla.org/media/css/jqueryui/moz-jquery-plugins.css" media="screen,projection,tv" rel="stylesheet"></link>
+<link href="https://developer.mozilla.org/media/css/sphinx.css" media="screen,projection,tv" rel="stylesheet"></link>
 
-  <!--[if lte IE 7]><link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_directory'); ?>/css/ie7.css" /><![endif]-->
-  <!--[if lte IE 6]><link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_directory'); ?>/css/ie6.css" /><![endif]-->
+<meta charset="utf-8">
+  <meta name="robots" content="index, follow">
+  <link rel="home" href="/en-US/">
+  <link rel="copyright" href="#copyright">
+  <link rel="shortcut icon" href="https://developer.cdn.mozilla.net/media/img/favicon.ico">
+
+
 
   <title><?php if (( is_single() || is_page() ) && (!is_front_page()) ) : ?><?php wp_title($sep = ''); ?> &#10025;
     <?php elseif ( is_search() ) : ?>Search results for &#8220;<?php the_search_query(); ?>&#8221; &#10025;
@@ -52,45 +89,87 @@
 
   <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 
-  <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-35433268-8']);
-    _gaq.push(['_trackPageview']);
-    (function(d, k) {
-      var ga = d.createElement(k); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == d.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = d.getElementsByTagName(k)[0]; s.parentNode.insertBefore(ga, s);
-    })(document, 'script');
-  </script>
 
   <?php wp_head(); ?>
-</head>
-
 <body <?php body_class(); ?>>
-<div id="outer-wrapper">
-  <ul id="nav-access" role="navigation">
-    <li><a href="#content-main">Skip to content</a></li>
-  </ul>
-
-  <header id="branding">
-    <?php if ( (is_front_page()) && ($paged < 1) ) { ?>
-    <h1 id="logo">hacks.mozilla.org</h1>
-    <?php } else { ?>
-    <h4 id="logo">hacks.mozilla.org <a href="<?php bloginfo('url'); ?>" title="Go to the home page">Home</a></h4>
-    <?php } ?>
-
-    <?php include (TEMPLATEPATH . '/searchform.php'); ?>
-
-    <nav id="nav-main">
-      <ul role="navigation">
-        <li <?php if ( (is_front_page()) && ($paged < 1) ) { ?>class="selected"<?php } ?>><a href="<?php bloginfo('url'); ?>">Home</a></li>
-        <li <?php if ( is_home() ) { ?>class="selected"<?php } ?>><a href="<?php echo get_permalink(get_page_by_path('articles')->ID); ?>">Articles</a></li>
-        <li <?php if ( is_page('demos') || is_category('demo') || is_category('featured-demo')) { ?>class="selected"<?php } ?>><a href="<?php echo get_permalink(get_page_by_path('demos')->ID); ?>">Demos</a></li>
-        <li <?php if ( is_page('about') ) {?>class="selected"<?php } ?>><a href="<?php echo get_permalink(get_page_by_path('about')->ID); ?>">About</a></li>
+<!--[if lte IE 8]>
+<noscript><div class="global-notice">
+<p><strong>Warning:</strong> The Mozilla Developer Network website employs emerging web standards that may not be fully supported in some versions of Microsoft Internet Explorer. You can improve your experience of this website by enabling JavaScript.</p></div></noscript>
+<![endif]-->
+  <header class="minor new-menu" id="masthead">
+    <div class="wrap">
+      <ul id="nav-access">
+        <li><a href="https://developer.mozilla.org/#language">Select language</a></li>
+                <li><a href="https://developer.mozilla.org/#content">Skip to main content</a></li>
       </ul>
-    </nav>
-    <a href="http://www.mozilla.org/" id="tabzilla">Mozilla</a>
-  </header><!-- /#branding -->
 
-  <div id="content">
-    
+      <div id="branding">
+                <div id="logo"><a href="https://developer.mozilla.org/"><img alt="Mozilla Developer Network" height="71" src="https://developer.mozilla.org/media/img/mdn-logo-sm.png" title="Mozilla Developer Network" width="62"></img> Mozilla Developer Network</a></div>
+              </div>
+              <nav id="nav">
+          <ul id="nav-main" role="menubar">
+            <li class="menu" id="nav-main-docs" rel="menuitem"><a aria-haspopup="true" aria-labelledby="nav-main-docs" class="toggle" href="https://developer.mozilla.org/#nav-sub-docs" title="Read Documentation">Read<br><em>Docs</em></a>
+              <div aria-hidden="true" class="sub-menu" id="nav-sub-docs">
+                <ul>
+                  <li>
+                    <ul>
+
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/HTML?menu">HTML</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/CSS?menu">CSS</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript?menu">JavaScript</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/Guide/Graphics?menu">Graphics</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/API?menu">APIs / DOM</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/Apps?menu">Apps</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/tools?menu">Dev Tools</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/MathML?menu">MathML</a></li>
+                    </ul>
+                  </li>
+                  <li>
+                    <ul>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/Tutorials?menu">Tutorials</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/Reference?menu">References</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Web/Guide?menu">Developer Guides</a></li>
+                      <li><a href="https://developer.mozilla.org/en-US/docs/Accessibility?menu">Accessibility</a></li>
+                      <li><a href="https://developer.mozilla.org/demos/?menu">Demos</a></li>
+                      <li><br><br><a href="https://developer.mozilla.org/docs?menu">...more docs</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </li>
+            <li id="nav-main-apps"><a href="https://marketplace.firefox.com/developers/?menu">Make<br><em>Apps</em></a>
+            </li>
+            <li class="menu" id="nav-main-firefox" role="menuitem"><a aria-haspopup="true" aria-labelledby="nav-main-firefox" class="firefox toggle" href="https://developer.mozilla.org/#nav-sub-firefox">Build &amp; Use<br><em>Firefox</em></a>
+              <ul class="sub-menu" id="nav-sub-firefox">
+                <li><a href="https://developer.mozilla.org/en-US/docs/Mozilla/Firefox_OS?menu">Firefox OS</a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Mozilla/Firefox?menu">Firefox Desktop</a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Mozilla/Mobile?menu">Mobile</a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons?menu">Add-ons</a></li>
+              </ul>
+            </li>
+            <li id="nav-main-demos" role="menuitem"><a class="demos" href="https://developer.mozilla.org/demos/?menu">See &amp; Submit<br><em>Demos</em></a></li>
+            <li class="menu" id="nav-main-community" role="menuitem"><a aria-haspopup="true" aria-labelledby="nav-main-community" class="community toggle" href="https://developer.mozilla.org/#nav-sub-community">Get<br><em>Involved</em></a>
+              <ul class="sub-menu" id="nav-sub-community">
+                <li><a href="https://developer.mozilla.org/en-US/docs/Project:MDN/Contributing/Join_the_community?menu">Join the Community</a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Project:MDN/Contributing?menu">Contribute to MDN</a></li>
+                <li><a href="https://developer.mozilla.org/events?menu">Events</a></li>
+                <li><a href="https://developer.mozilla.org/en-US/docs/Project:MDN/Contributing/Follow_what_s_happening?menu">Tweets, Blogs, and More</a></li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <a href="//www.mozilla.org/" id="tabzilla">mozilla</a>
+    </div>
+  </header>
+  <!-- top toolbar -->
+  <section id="nav-toolbar"><div><div class="wrap">
+
+    <!-- left crumb navigation -->
+    <nav class="crumbs" role="navigation">
+      <ol>
+          <li class="crumb"><a href="https://developer.mozilla.org/{{ pathto(master_doc) }}">{{ docstitle }}</a></li>
+          <li class="crumb">{{ title }}</li>
+      </ol>
+    </nav>
+
+  </div></div></section>
